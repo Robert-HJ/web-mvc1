bc3c1841ffb9401151b9aa0e820bfcff7c31dea0
robert <roberthj329@gmail.com>
2024-10-05 16:39:19 +0900

    김영한 Web MVC 1 - 24/10/05

---
+++ src/main/java/hello/servlet/domain/web/frontcontroller/v1/controller/MemberControllerV1.java
@@ -0,0 +1,16 @@
+package hello.servlet.domain.web.frontcontroller.v1.controller;
+
+import hello.servlet.domain.web.frontcontroller.v1.ControllerV1;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MemberControllerV1 implements ControllerV1 {
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String viewPath = "/WEB-INF/views/new-form.jsp";
+
+    }
+}
---
+++ src/main/java/hello/servlet/domain/web/servletmvc/MvcMemberFormServlet.java
@@ -0,0 +1,8 @@
+package hello.servlet.domain.web.servletmvc;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(name = "mvcMemberFormServlet")
+public class MvcMemberFormServlet extends HttpServlet {
+}
